fe4dda9b1b2fc1f4cfac297c04625fd2ac4332cb
Must_nu73@itstep.edu.az <Must_nu73@itstep.edu.az>
2021-12-21 12:28:51 +0400

    Add project files.

---
+++ Clients/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Clients.Command;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Clients.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainWindow MainView { get; set; }
+
+        public RelayCommand DownloadCommand { get; set; }
+
+        public RelayCommand SendCommand { get; set; }
+        public MainViewModel()
+        {
+            DownloadCommand = new RelayCommand((sender) =>
+            {
+                //OpenFileDialog openFile = new OpenFileDialog();
+                //openFile.Filter = "Txt File (*.txt)|*.txt";
+                //openFile.Title = "Select File";
+
+                MessageBox.Show("Salam");
+                
+            });
+
+            SendCommand = new RelayCommand((sender) =>
+            {
+
+            });
+        }
+    }
+}
---
+++ SendImageToServer/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendImageToServer.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainWindow MainView { get; set; }
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
