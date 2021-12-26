b9404f451040559e6cb8bdcf66824795da15b0fd
Must_nu73@itstep.edu.az <Must_nu73@itstep.edu.az>
2021-12-26 16:08:06 +0400

    Finish Client Side

--- Clients/ViewModels/MainViewModel.cs
+++ Clients/ViewModels/MainViewModel.cs
@@ -3,9 +3,15 @@ using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
 
 namespace Clients.ViewModels
 {
@@ -16,32 +22,75 @@ namespace Clients.ViewModels
         public RelayCommand DownloadCommand { get; set; }
 
         public RelayCommand SendCommand { get; set; }
+
+
+        private string _imagePath { get; set; }
+
+
+        private Image _image;
+        public Image Image
+        {
+            get { return _image; }
+            set { _image = value; OnPropertyChanged(); }
+        }
+
         public MainViewModel()
         {
             DownloadCommand = new RelayCommand((sender) =>
             {
-                //// open file dialog   
-                //OpenFileDialog open = new OpenFileDialog();
-                //// image filters  
-                //open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
-                //if (open.ShowDialog() == DialogResult.OK)
-                //{
-                //    // display image in picture box  
-                //    pictureBox1.Image = new Bitmap(open.FileName);
-                //    // image file path  
-                //    textBox1.Text = open.FileName;
-                //}
-
-                OpenFileDialog openFileDialog1 = new OpenFileDialog();
-                openFileDialog1.DefaultExt = "png";
-                openFileDialog1.ShowDialog();
-
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "PNG (*.png)|*.png|JPEG (*.jpeg)|*.jpeg|JPG (*.jpg)|*.jpg";
+                open.FilterIndex = 1;
+                open.Multiselect = false;
+                if (Convert.ToBoolean(open.ShowDialog()) == true)
+                {
+                    MainView.MyImage.Source = new BitmapImage(new Uri(open.FileName));
+                    _imagePath = open.FileName;
+                }
             });
+
+            Image = new Image();
 
             SendCommand = new RelayCommand((sender) =>
             {
-
+                Thread thread = new Thread(() =>
+                {
+                    Send();
+                });
+                thread.Start();
+            },(predicate) =>
+            {
+                if (MainView.MyImage.Source != null)
+                {
+                    return true;
+                }
+                return false;
             });
+        }
+        private async void Send()
+        {
+            var ipadress = IPAddress.Parse("192.168.1.109");
+            var port = 2501;
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var ep = new IPEndPoint(ipadress, port);
+                try
+                {
+                    socket.Connect(ep);
+                    if (socket.Connected)
+                    {
+                        var bytes = Encoding.UTF8.GetBytes(_imagePath);
+                        socket.Send(bytes);
+                        MessageBox.Show("Image is send succesfully");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
         }
     }
 }
--- SendImageToServer/ViewModels/MainViewModel.cs
+++ SendImageToServer/ViewModels/MainViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace SendImageToServer.ViewModels
 {
@@ -10,9 +16,61 @@ namespace SendImageToServer.ViewModels
     {
         public MainWindow MainView { get; set; }
 
+        private Image _serverImage;
+
+        public Image ServerImage
+        {
+            get { return _serverImage; }
+            set { _serverImage = value; OnPropertyChanged(); }
+        }
+
         public MainViewModel()
         {
+            Receive();
+        }
 
+        private void Receive()
+        {
+            var ipadress = IPAddress.Parse("192.168.1.109");
+            var port = 2501;
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var ep = new IPEndPoint(ipadress, port);
+                socket.Bind(ep);
+                socket.Listen(10);
+                ServerImage = new Image();
+                while (true)
+                {
+                    var client = socket.Accept();
+
+                    var bytes = new byte[1024];
+                    int lenght = 0;
+                    lenght = client.Receive(bytes);
+                    var data = new byte[lenght];
+                    Array.Copy(bytes, 0, data, 0, lenght);
+                    bytes = new byte[lenght];
+                    Array.Copy(data, 0, bytes, 0, lenght);
+                    ServerImage = ByteArrayToImage(bytes);
+
+                }
+            }
+
+        }
+
+
+        public Image ByteArrayToImage(byte[] buffer)
+        {
+            Image returnImage = null;
+            string path = Encoding.UTF8.GetString(buffer);
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                returnImage = new Image();
+                returnImage.Source = new BitmapImage(new Uri(path));
+            });
+
+            return returnImage;
         }
     }
 }
